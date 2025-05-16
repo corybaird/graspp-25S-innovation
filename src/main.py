23e1d51d7b423e6f8d7669c7d5a019bf000f74f6
Joseph Chen <happypoetjo@gmail.com>
2025-05-16 13:41:36 +0900

    Revert "made font path as parameter"
    This reverts commit 258bd49bd2e88846a6ca456d5462ae4e1a8d0e25.

--- src/main.py
+++ src/main.py
@@ -314,9 +314,8 @@ class DataCleaner:
     """
     A class to clean and process downloaded EXCEL files.
     """
-    def __init__(self, download_dir, font_path='/Library/Fonts/Arial Unicode.ttf'):
+    def __init__(self, download_dir):
         self.download_dir = download_dir
-        self.font_path = font_path # visualization use 
         self.research_expense_cleaner = ResearchExpenseCleaner(download_dir)
         self.patent_count_cleaner = PatentCountCleaner(download_dir)
         
@@ -359,11 +358,7 @@ class DataCleaner:
         from matplotlib.font_manager import FontProperties
         # 自分のパソコンにインストールされている日本語フォントを指定してください
         # For MacOS
-        try:
-            fp = FontProperties(fname = self.font_path,size = 11)
-        except Exception as e:
-            print(f"Error loading font: {e}")
-            return
+        fp = FontProperties(fname = '/Library/Fonts/Arial Unicode.ttf',size = 11)
         plt.rc('font', family=fp.get_name())
         # 1: Top 10 Industries by Total R&D Costs (2020)
         df_2020 = self.ResearchExpenseDict[year]
@@ -595,7 +590,7 @@ def main():
     # Run scraper
     print("Starting data scraping...")
     scraper = DataScraper(BASE_URLs_scrape, DOWNLOAD_DIR, years_scrape)
-    scraper.run_scraper()
+#    scraper.run_scraper()
     print("Data scraping complete.")
 
     # Run data cleaning
